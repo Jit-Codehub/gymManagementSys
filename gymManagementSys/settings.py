4c6ebdca8abdd29544e0da129c5b1ab41b51a63b
Jitender Kumar <jkcodingjk@gmail.com>
2023-02-03 00:08:26 +0530

    admin panel UI updated

--- gymManagementSys/settings.py
+++ gymManagementSys/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR/'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "GYM Dashboard",
+    "site_brand": "GYM Dashboard",
+    }
