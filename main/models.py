08d4987d26fdedb25a01537eda394b41a90b2818
Jitender Kumar <jkcodingjk@gmail.com>
2023-02-23 08:34:08 +0530

    changes done in dashboard

--- main/models.py
+++ main/models.py
@@ -143,6 +143,9 @@ class Trainer(models.Model):
     is_active = models.BooleanField(default=False)
     detail = models.TextField()
     img=models.ImageField(upload_to="trainers/")
+    facebook = models.CharField(max_length=100,null=True)
+    twitter = models.CharField(max_length=100,null=True)
+    youtube = models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return self.full_name
